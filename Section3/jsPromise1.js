e72d2b5400e015363fd8c7153fc8d4b68d1ac745
stoyosawa <s.toyosawa@f5.com>
2020-07-26 15:36:40 +1200

    Update

--- Section3/jsPromise1.js
+++ Section3/jsPromise1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
 // 2020-05-16
 
@@ -24,30 +23,3 @@ promise
   .finally(function() {
     console.log('Finally');
   });
-=======
-#!/usr/bin/env node
-// 2020-05-16
-
-const fs = require('fs');
-
-let promise = new Promise(function(resolve, reject) {
-  fs.writeFile('default.txt', 'hello world', function(err) {
-    if (err) {
-      reject(err);
-      return;
-    }
-    resolve('Written to the file');
-  });
-});
-
-promise
-  .then(function(data) {
-    console.log(`Success: ${data}`);
-  })
-  .catch(function(err) {
-    console.log(`Failed: ${err.toString()}`);
-  })
-  .finally(function() {
-    console.log('Finally');
-  });
->>>>>>> 1a514557b46c4ddda9ee781c87c6b6845aac47ad
